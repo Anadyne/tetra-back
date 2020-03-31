c6eb0e0ad583e2d390d1c3382ad2c889c943999e
tampler <socnetfpga@gmail.com>
2020-04-01 01:06:04 +0300

    Upd main

--- project/plugins.sbt
+++ project/plugins.sbt
@@ -1,5 +1,5 @@
 addSbtPlugin("org.scalameta"             % "sbt-scalafmt"        % "2.3.2")
-addSbtPlugin("ch.epfl.scala"             % "sbt-scalafix"        % "0.9.12")
+addSbtPlugin("ch.epfl.scala"             % "sbt-scalafix"        % "0.9.13")
 addSbtPlugin("io.github.davidgregory084" % "sbt-tpolecat"        % "0.1.11")
 addSbtPlugin("com.typesafe.sbt"          % "sbt-native-packager" % "1.7.0")
 addSbtPlugin("io.spray"                  % "sbt-revolver"        % "0.9.1")
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -11,18 +11,17 @@ import org.http4s.server.blaze.BlazeServerBuilder
 import org.http4s.server.middleware.CORS
 import org.http4s.server.middleware.Logger
 
-import cats.effect.ExitCode
+import cats.effect.{ ConcurrentEffect, ExitCode }
 import sttp.tapir.docs.openapi._
 import sttp.tapir.openapi.circe.yaml._
 import sttp.tapir.swagger.http4s.SwaggerHttp4s
 import types._
 
-import zio.clock.Clock
-import zio.console.putStrLn
 import zio.interop.catz._
-import zio.{ RIO, ZEnv, ZIO }
+import zio.{ Runtime, ZEnv, ZIO }
 
-object Main extends CatsApp {
+object Main extends App {
+  val rt = Runtime.default
 
   private val userRoute = new UserRoute[AppEnvironment]
   private val yaml      = userRoute.getEndPoints.toOpenAPI("User", "1.0").toYaml
@@ -32,20 +31,16 @@ object Main extends CatsApp {
 
   val env = ZEnv.live ++ UserRepository.live ++ AppLogger.live
 
-  def runHttp[R <: Clock](app: HttpApp[AppTask], cfg: AppConfig): RIO[AppEnvironment, Unit] =
-    ZIO.runtime[AppEnvironment].flatMap { implicit rts =>
-      BlazeServerBuilder[AppTask]
-        .bindHttp(cfg.server.port, cfg.server.host)
-        .withHttpApp(CORS(app))
-        .serve
-        .compile[AppTask, AppTask, ExitCode]
-        .drain
-    }
+  def runHttp(app: HttpApp[AppTask], cfg: AppConfig) = { implicit rts: ConcurrentEffect[AppTask] =>
+    BlazeServerBuilder[AppTask]
+      .bindHttp(cfg.server.port, cfg.server.host)
+      .withHttpApp(CORS(app))
+      .serve
+      .compile[AppTask, AppTask, ExitCode]
+      .drain
+  }
 
-  val prog: ZIO[AppEnvironment, Serializable, Unit] =
-    ZIO.fromEither(loadConfig).flatMap(cfg => runHttp(finalHttpApp, cfg))
+  val prog = ZIO.fromEither(loadConfig).map(_ => runHttp(finalHttpApp, _)) //.provideCustomLayer(env)
 
-  override def run(args: List[String]) =
-    prog.provideLayer(env).foldM(err => putStrLn(s"Execution failed with: $err").as(1), _ => ZIO.succeed(0))
-
+  rt.unsafeRun(prog)
 }
