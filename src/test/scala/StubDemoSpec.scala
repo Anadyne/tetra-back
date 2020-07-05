3dd92047526ecbac3ab33d869eb019221fd499e1
tampler <socnetfpga@gmail.com>
2020-07-05 14:46:49 +0300

    Fixed errors

--- src/test/scala/StubDemoSpec.scala
+++ src/test/scala/StubDemoSpec.scala
@@ -7,25 +7,24 @@ import org.fsf.tetra.route.Endpoints._
 import sttp.client.Identity
 import sttp.client._
 import sttp.client.monad.MonadError
-import sttp.client.monad._
+import sttp.client.monad.{ IdMonad }
 import sttp.client.testing.SttpBackendStub
 import sttp.tapir.client.sttp._
-import sttp.tapir.server.stub._
 
 import zio.test.Assertion._
 import zio.test._
+import sttp.model._
 
 object StubDemoSpec extends DefaultRunnableSpec {
   def spec = suite("Endpoints Spec")(
     test("Validate getUserEndpoint") {
 
-      implicit val backend = SttpBackendStub
+      implicit val backend = SttpBackendStub[Identity, Nothing, NothingT](idMonad)
       // .withFallback(???)
-        .apply(idMonad)
         .whenRequestMatches(_.uri.path.startsWith(List("a", "b")))
         .thenRespond("Hello there!")
-        .whenRequestMatches(getUserEndpoint)
-        .thenSuccess(user)
+        .whenRequestMatches(_.method == Method.GET)
+        .thenRespond(user)
 
       val resp  = getUserEndpoint.toSttpRequestUnsafe(uri"http://test.com").apply(11).send()
       val resp1 = basicRequest.get(uri"http://example.org/a/b/c").send()
