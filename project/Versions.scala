72035e6df8f729128b48d2780385f9209872f37d
tampler <socnetfpga@gmail.com>
2020-08-03 13:03:34 +0300

    Bump vers

--- project/Versions.scala
+++ project/Versions.scala
@@ -1,11 +1,11 @@
 object Version {
   val zio            = "1.0.0-RC21-2"
-  val zioInteropCats = "2.1.3.0-RC16"
+  val zioInteropCats = "2.1.4.0-RC17"
 
-  val http4s = "1.0.0-M3"
-  val tapir  = "0.16.1"
-  val sttp   = "2.2.1"
-  val circe  = "0.13.0"
+  val http4s = "0.21.6"
+  val tapir  = "0.16.10"
+  val sttp   = "2.2.3"
+  val circe  = "0.14.0-M1"
 
   val quill  = "3.5.2"
   val h2db   = "1.4.200"
